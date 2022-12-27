53fae3627b1be6c93362cad826c9c0c3978dee6f
jose <jose.rafael1611@gmail.com>
2022-12-27 13:37:37 +0700

    create class user

---
+++ app/src/main/java/com/example/itplnsehat/user.kt
@@ -0,0 +1,12 @@
+package com.example.itplnsehat
+
+class user {
+    var nama : String = ""
+    var nomor : String = ""
+    var password : String = ""
+    constructor(nama : String, nomor : String , password : String ){
+        this.nama = nama
+        this.nomor = nomor
+        this.password = password
+    }
+}
