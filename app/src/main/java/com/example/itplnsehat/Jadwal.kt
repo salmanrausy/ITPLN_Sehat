603043159f61422891372d712e6bc210f289cc9b
jose <jose.rafael1611@gmail.com>
2022-12-27 13:50:02 +0700

    create class user

---
+++ app/src/main/java/com/example/itplnsehat/Jadwal.kt
@@ -0,0 +1,12 @@
+package com.example.itplnsehat
+
+class Jadwal {
+    var id_user : Int = 0
+    var id_dokter : Int = 0
+    var keterangan : String = ""
+     constructor(idUser : Int , idDokter : Int , keterangan : String){
+         this.id_user = idUser
+         this.id_dokter = idDokter
+         this.keterangan = keterangan
+     }
+}
