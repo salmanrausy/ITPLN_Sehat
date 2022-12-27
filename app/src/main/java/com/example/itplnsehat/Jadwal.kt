2480744c8e1e043f831ba7c2f16624e71da80421
jose <jose.rafael1611@gmail.com>
2022-12-27 13:51:03 +0700

    create class Jadwal

--- app/src/main/java/com/example/itplnsehat/Jadwal.kt
+++ app/src/main/java/com/example/itplnsehat/Jadwal.kt
@@ -4,7 +4,7 @@ class Jadwal {
     var id_user : Int = 0
     var id_dokter : Int = 0
     var keterangan : String = ""
-     constructor(idUser : Int , idDokter : Int , keterangan : String){
+    constructor(idUser : Int , idDokter : Int , keterangan : String){
          this.id_user = idUser
          this.id_dokter = idDokter
          this.keterangan = keterangan
