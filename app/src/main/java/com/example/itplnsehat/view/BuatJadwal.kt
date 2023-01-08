c6d5d7b4b70e4a6243cea9a64bcb864e2c4901b5
Salman Rausyan Fikri <salmanrausy@gmail.com>
2023-01-09 02:09:14 +0700

    update datePicker at activity_buat_jadwal.xml

--- app/src/main/java/com/example/itplnsehat/view/BuatJadwal.kt
+++ app/src/main/java/com/example/itplnsehat/view/BuatJadwal.kt
@@ -7,14 +7,39 @@ import android.view.View
 import android.widget.*
 import com.example.itplnsehat.R
 import kotlinx.android.synthetic.main.activity_buat_jadwal.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class BuatJadwal : AppCompatActivity(){
+    private lateinit var tvDatePicker: TextView
+    private lateinit var btnDatePicker: Button
 
     val list_Doctor = arrayOf("Dr. Banner", "Dr. Kamaludin", "Dr. Kehfa", "Dr. Lucille", "Dr. Meisya", "Dr. Strange")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buat_jadwal)
+
+        //Find Id Button
+        btnDatePicker = findViewById<Button>(R.id.btn_DatePickerJadwal) //mencari id Button 'cardDatePicker' di file activity_buat_jadwal
+
+        //Find id TextView
+        val myCalendar = Calendar.getInstance()
+        tvDatePicker = findViewById(R.id.btn_DatePickerJadwal) //mencari id Button 'born' di file activity_daftar
+
+        val datePicker = DatePickerDialog.OnDateSetListener { view, year, month,
+                                                              dayofmonth ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayofmonth)
+            updateLable(myCalendar)
+        }
+        btnDatePicker.setOnClickListener{
+            DatePickerDialog(this, datePicker,
+                myCalendar.get(Calendar.YEAR),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
 
         val spinner = findViewById<Spinner>(R.id.spinner)
         val arrayAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, list_Doctor)
@@ -35,6 +60,17 @@ class BuatJadwal : AppCompatActivity(){
             }
 
         }
+    }
+
+    private fun updateLable(myCalendar: Calendar){
+        val myformat = "dd-MM-yyyy"
+        val sdf = SimpleDateFormat(myformat, Locale.UK)
+        tvDatePicker.setText("Jadwal Anda : " + sdf.format(myCalendar.time))
+    }
+    private fun getDate(myCalendar: Calendar): String{
+        val myformat = "yyyy-MM-dd"
+        val sdf = SimpleDateFormat(myformat, Locale.UK)
+        return sdf.format(myCalendar.time)
     }
 
 }
